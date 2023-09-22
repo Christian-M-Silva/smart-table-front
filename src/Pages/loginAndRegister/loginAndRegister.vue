da41736ea4cfef603f9bf545e571910e1a049743
Christian-M-Silva <tininjacms@gmail.com>
2023-09-22 07:23:51 -0300

    feat(addBlockInRegister): Bloqueará o botão cadastro
    O cadastro só será permitido para quem inserio uma entity.

--- src/Pages/loginAndRegister/loginAndRegister.vue
+++ src/Pages/loginAndRegister/loginAndRegister.vue
@@ -28,6 +28,7 @@
                   class="bg-slate-700 text-white hover:bg-slate-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
                   type="button"
                   @click="iniciarAutenticacao"
+                  :disabled="action === 'Cadastro' && !entity"
                 >
                   Faça seu {{ action }}
                 </button>
